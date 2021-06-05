6b84b1b246907a4313870a9e7a049fa390653658
kk <zikewang@outlook.com>
2021-06-06 00:43:33 +0800

    finish the prototype of a in-memory, append-only, single-table db

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdbool.h>
 #include <string.h> // strcmp()
 #include <stdlib.h> // exit(); #define EXIT_SUCCESS 0, #define EXIT_FAILURE 1
+#include <stdint.h> // uint32_t
 
+// store user inputs
 typedef struct {
     char* buffer;
     size_t buffer_length; // unsigned
@@ -16,17 +18,122 @@ typedef enum {
 
 typedef enum {
     PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_SYNTAX_ERROR
 } PrepareResult;
+
+typedef  enum {
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} ExecuteResult;
 
 typedef enum {
     STATEMENT_SELECT,
     STATEMENT_INSERT
 } StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+// row structure
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+// user input statement
 typedef struct {
     StatementType type;
+    Row row_to_insert; // store the parameters in the insert statement
 } Statement;
+
+// macro to calculate the size of a member in a specific struct
+#define size_of_member(Type, Member) sizeof(((Type*)0)->Member)
+
+// define the size of the members in the Row struct
+const uint32_t ID_SIZE = size_of_member(Row, id);
+const uint32_t USERNAME_SIZE = size_of_member(Row, username);
+const uint32_t EMAIL_SIZE = size_of_member(Row, email);
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+// define the address offset of the members in the Row struct
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+
+// Compactly store each member of Row-struct according to the offset
+void serialize_row(Row* source, void* destination) {
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+// Convert compact representation to a Row-struct instance
+void deserialize_row(void* source, Row* destination) {
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+// define constants related to the size and number of pages in memory
+#define TABLE_MAX_PAGES 100 // arbitrary limit of pages we can allocate
+const uint32_t PAGE_SIZE = 4096; // 4KB, consistent with the page size of virtual memory in os
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = TABLE_MAX_PAGES * ROWS_PER_PAGE;
+
+// keeps track of how many rows there are and pointers to each page
+typedef struct {
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+} Table;
+
+// create a Table and allocate memory
+Table* new_table() {
+    Table* table = malloc(sizeof(Table));
+
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; ++i) {
+        table->pages[i] = NULL; // remember to initialize pointer
+    }
+
+    return table;
+}
+
+// In this compact representation,
+// (1) the row number and ROWS_PER_PAGE can be used to locate which page in the Table the row is on,
+// (2) and furthermore, the position of the row in the page can be known (actually, it is a remainder),
+// (3) and each member can be accessed in memory by the offset of the members in the row.
+// This function only completes the first & second steps, which is to return the memory address of a row.
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE; // complete step(1)
+
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        page = table->pages[page_num] = malloc(PAGE_SIZE); // Allocate memory if the page doesn't exist yet
+    }
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE; // complete step(2), offset on row numbers
+    uint32_t byte_offset = row_offset * ROW_SIZE; // converts offset on row numbers to offset on memory bytes
+
+    return page + byte_offset; // row start address = page start address + page offset
+}
+
+// release memory allocated to Table
+void free_table(Table* table) {
+    // It's tricky here, because according to the implementation in row_slot,
+    // pages are assigned according to the number of rows, and the loop exits
+    // when the first empty page is encountered("table->pages[i] == NULL")
+    for (uint32_t i = 0; table->pages[i]; ++i) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
+// refer to sqlite's prompt
+void print_prompt() {
+    printf("db > ");
+}
 
 // initialize an InputBuffer instance
 InputBuffer* new_input_buffer() {
@@ -37,11 +144,6 @@ InputBuffer* new_input_buffer() {
     input_buffer->input_length = 0;
 
     return input_buffer;
-}
-
-// refer to sqlite's prompt
-void print_prompt() {
-    printf("db > ");
 }
 
 // Get the user input statements and store them in a custom memory buffer
@@ -80,31 +182,67 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 
 // hacky version of the "SQL Compiler"
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
-    if (strncmp(input_buffer->buffer, "select", 6) == 0) {
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        // In this implementation, "select" is a complete command with no additional
+        // arguments to print all rows,so there is no need to use strncmp
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
     }
     else if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        // In this implementation, "insert x y z" takes three parameters,
+        // which are id, username and email, so we need to use strncmp
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                                   statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assigned != 3) {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
 
     return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
+// print row data
+void print_row(Row* row) {
+    printf("(%d, %s, %s)\n", row->id, row->username, row->email);
+}
+
+// execution of a "select" statement
+ExecuteResult execute_select(Statement* statement, Table* table) {
+    Row row;
+    for (uint32_t i = 0; i < table->num_rows; ++i) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return EXECUTE_SUCCESS;
+}
+
+// execution of a "insert x y z" statement
+ExecuteResult execute_insert(Statement* statement, Table* table) {
+    if (table->num_rows >= TABLE_MAX_ROWS) {
+        return EXECUTE_TABLE_FULL;
+    }
+
+    Row* row = &(statement->row_to_insert);
+    serialize_row(row, row_slot(table, table->num_rows));
+    table->num_rows += 1;
+
+    return EXECUTE_SUCCESS;
+}
+
 // hacky version of the virtual machine
-void execute_statement(Statement* statement) {
+ExecuteResult execute_statement(Statement* statement, Table* table) {
     switch (statement->type) {
         case (STATEMENT_SELECT) :
-            printf("TODO: this is where we would do a select.\n");
-            break;
+            return execute_select(statement, table);
         case (STATEMENT_INSERT) :
-            printf("TODO: this is where we would do a insert.\n");
-            break;
+            return execute_insert(statement, table);
     }
 }
 
 int main(int argc, char* argv[]) {
+    Table* table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
 
     while (true) {
@@ -131,10 +269,19 @@ int main(int argc, char* argv[]) {
             case (PREPARE_UNRECOGNIZED_STATEMENT) :
                 printf("Error: unknown statement: '%s'.\n", input_buffer->buffer);
                 continue;
+            case (PREPARE_SYNTAX_ERROR) :
+                printf("Syntax error, can't parse statement: '%s'.\n", input_buffer->buffer);
+                continue;
         }
 
         // Executing SQL statement
-        execute_statement(&statement);
-        printf("Executed.\n");
+        switch (execute_statement(&statement, table)) {
+            case (EXECUTE_SUCCESS) :
+                printf("Executed.\n");
+                break;
+            case (EXECUTE_TABLE_FULL) :
+                printf("Error: Table full.\n");
+                break;
+        }
     }
 }
